04185355bad0c9b2a274e14550d63dd9071ce783
Lucas <l.gilhard@gmail.com>
2020-04-12 17:17:35 +0200

    improve PersonCard ui

--- lib/ui/home/about_us_section.dart
+++ lib/ui/home/about_us_section.dart
@@ -23,15 +23,15 @@ class AboutUsSection extends StatelessWidget {
             maxWidth: MQ.xlMaxWidth,
           ),
           child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            padding: const EdgeInsets.symmetric(vertical: 65.0),
             child: Wrap(
               alignment: WrapAlignment.center,
               spacing: 50.0,
-              runSpacing: 40.0,
+              runSpacing: 80.0,
               children: _persons
                   .map((Person person) => ConstrainedBox(
                         constraints: BoxConstraints.tightFor(
-                            width: 220.0, height: 350.0),
+                            width: 220.0, height: 380.0),
                         child: PersonCard(
                           name: person.name,
                           age: person.age,
@@ -67,38 +67,87 @@ class PersonCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 8.0,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16.0),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 20.0,
-                bottom: 4.0,
-              ),
-              child: PersonNameText(name),
+    return Stack(
+      overflow: Overflow.visible,
+      children: <Widget>[
+        Card(
+          elevation: 8.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 8.0,
+              top: 50.0,
+              right: 8.0,
+              bottom: 8.0,
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: PersonMetaText('$age ans - $location'),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 20.0,
+                    bottom: 4.0,
+                  ),
+                  child: PersonNameText(name),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: PersonMetaText('$age ans - $location'),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Center(
+                      child: ContentText(presentation),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Center(
-                  child: ContentText(presentation),
+          ),
+        ),
+        Positioned(
+          top: -50.0,
+          left: 0.0,
+          right: 0.0,
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: Container(
+                  height: 0.0,
                 ),
               ),
-            ),
-          ],
+              Flexible(
+                flex: 2,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey[50],
+                      width: 5.0,
+                    ),
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: ClipOval(
+                      child: Image.asset(picture),
+                    ),
+                  ),
+                ),
+              ),
+              Flexible(
+                flex: 1,
+                child: Container(
+                  height: 0.0,
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+      ],
     );
   }
 }
